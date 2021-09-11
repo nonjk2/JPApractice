de084a7b27d94d71da0b7c98b5b6bb19f72a6079
nonjk2 <trgf456@naver.com>
2021-09-11 16:32:00 +0900

    JPA 1-1
    H2 DATABASE 사용
    JPA entityManagerfactory 사용
    persistence.xml 생성
    CRUD 사용법등

---
+++ JPApractice/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,69 @@
+package hellojpa;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");/* hello = persistence.xml*/
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        /*try {
+            //====== 맴버 insert 할때! ========//
+            Member member = new Member();
+
+            member.setId(2L);
+            member.setName("HelloB");
+
+            em.persist(member);
+
+            tx.commit();
+        }*/
+
+        /*try {
+            //====== 맴버 select 할때! ========//
+            Member findMember = em.find(Member.class, 1L);
+            System.out.println("findMember.id=" + findMember.getId());
+            System.out.println("findMember.name=" + findMember.getName());
+
+            tx.commit();
+        }*/
+
+        /*try {
+            //====== 맴버 delete 할때! ========//
+            Member findMember = em.find(Member.class, 1L);
+
+            em.remove(findMember); //1L 삭제
+
+            tx.commit();
+        }*/
+
+        /*try {
+            //====== 맴버 update 할때! ========//
+            Member findMember = em.find(Member.class, 1L);
+            findMember.setName("HelloJPA");
+            tx.commit();
+        }*/
+
+        try {
+            List<Member> result = em.createQuery("select m from Member as m",Member.class)
+                    .setFirstResult(0)
+                    .setMaxResults(5)
+                    .getResultList();
+            for (Member member : result ){
+                System.out.println("member.name = "+member.getName());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
+
